4b1b1169b465620582780438c6db24849dfb29b6
Maria Alfaro <maria.alfaro@mail.escuelaing.edu.co>
2021-04-08 13:40:12 -0500

    Update apiclient.js

--- src/main/resources/static/apiclient.js
+++ src/main/resources/static/apiclient.js
@@ -1,11 +1,12 @@
 apiclient = (function () {
     let localhost = "http://localhost:8080";
+    let despliegue = "https://arsw-weather.herokuapp.com";
 
     function getWeatherByCity(city_name, callback) {
 
         const promise = new Promise((resolve, reject) => {
             $.ajax({
-                url: localhost + "/weather/" + city_name,
+                url: despliegue + "/weather/" + city_name,
                 type: 'GET',
                 contentType: "application/json"
             }).done(function (response) {
